c17b4143e1cd9dcd5fb8ecfc9f10af14703061e3
Logicalyan <adrianmuhammad2408@gmail.com>
2025-08-29 14:37:34 +0700

    validation: email feat register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\Role;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -18,28 +19,37 @@ class AuthController extends Controller
 
         $validated = $request->validate([
             'name' => 'required|string|max:10',
-            'email' => 'required|string|email',
+            'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:6'
         ]);
 
-        $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => Hash::make($validated['password']),
-        ]);
+        DB::beginTransaction();
 
-        $role = Role::where('slug', 'customer')->first();
-        $role && $user->roles()->attach($role->id);
+        try {
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]);
 
-        //Device
-        $deviceName = $request->header('User-Agent') ?? 'unknown device';
+            $role = Role::where('slug', 'customer')->first();
+            $role && $user->roles()->attach($role->id);
 
-        //Buat token
-        $token = $user->createToken(
-            $deviceName,
-            ['*'],
-            Carbon::now()->addDays(7)
-        )->plainTextToken;
+            //Device
+            $deviceName = $request->header('User-Agent') ?? 'unknown device';
+
+            //Buat token
+            $token = $user->createToken(
+                $deviceName,
+                ['*'],
+                Carbon::now()->addDays(7)
+            )->plainTextToken;
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->error('Registration Failed: ' . $e->getMessage(), 500);
+        }
 
         return $this->success([
             'user' => $user,
