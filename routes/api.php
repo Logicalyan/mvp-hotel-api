002342c6a13b6ad39b8d3ec7e8770d0b5eba9ab2
Candrabirawa29 <damarproject10@gmail.com>
2025-09-09 22:39:31 +0700

    feat: role middleware

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,15 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('users', UserController::class);
+    });
+});
+
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
     Route::get('/user', 'profile')->middleware('auth:sanctum');
     Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
-
-Route::resource('users', UserController::class);
