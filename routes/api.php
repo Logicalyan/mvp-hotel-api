d20287e7ac2dce3e9adbcd2edaee16b96959e8df
Logicalyan <adrianmuhammad2408@gmail.com>
2025-09-19 23:36:37 +0700

    feat: CRUD Users done!

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Data\Hotel\Reference\ReferenceController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomTypeController;
 use App\Http\Controllers\Data\BedTypeController;
-
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +16,7 @@ Route::get('/cities', [ReferenceController::class, 'cities']);
 Route::get('/districts', [ReferenceController::class, 'districts']);
 Route::get('/sub-districts', [ReferenceController::class, 'subDistricts']);
 Route::get('/facilities', [ReferenceController::class, 'facilities']);
+Route::get('/roles', [RoleController::class, 'index']);
 Route::apiResource('hotels', HotelController::class);
 Route::apiResource('users', UserController::class);
 Route::apiResource('bed-types', BedTypeController::class);
